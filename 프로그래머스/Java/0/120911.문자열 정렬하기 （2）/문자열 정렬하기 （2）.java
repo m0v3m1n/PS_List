d07ffd8227ee23d7637ff69f9483b9150efeef20
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2026-08-06 00:07:53 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 7.72 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/Java/0/120911.문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution
+{
+    public String solution(String my_string)
+    {
+        return my_string.toLowerCase().chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
+}
