55f7109f2799602e7225f1bcc23e14f79741e8fd
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2026-08-06 13:55:48 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/Java/0/181839.주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,7 @@
+class Solution
+{
+    public int solution(int a, int b)
+    {
+        return a % 2 == 1 && b % 2 == 1 ? a * a + b * b : a % 2 == 1 || b % 2 == 1 ? 2 * (a + b) : a > b ? a - b : b - a;
+    }
+}
