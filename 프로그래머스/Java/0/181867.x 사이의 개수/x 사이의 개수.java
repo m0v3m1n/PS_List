162f95912f6c350a9d00034d0604028efc6f9aa5
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2026-08-10 18:14:28 +0900

    [level 0] Title: x 사이의 개수, Time: 8.90 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/Java/0/181867.x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String myString)
+    {
+        return Arrays.stream(myString.split("x", -1))
+            .mapToInt(String::length)
+            .toArray();
+    }
+}
