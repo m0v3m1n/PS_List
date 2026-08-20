d32cdaa1653ea0e2e6f678ab789b01b1326f94a8
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2026-08-20 12:17:22 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.93 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/Java/0/181879.길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int[] num_list)
+    {
+        if(num_list.length > 10)
+            return Arrays.stream(num_list).sum();
+        else
+        {
+            int result = 1;
+            
+            for(int i : num_list)
+                result *= i;
+            
+            return result;
+        }
+    }
+}
