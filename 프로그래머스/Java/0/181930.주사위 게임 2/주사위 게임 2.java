243066432ccc22ee12d87fafe873720dba1239cc
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2026-08-31 11:46:30 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/Java/0/181930.주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,8 @@
+class Solution
+{
+    public int solution(int a, int b, int c)
+    {
+        
+        return a != b && a != c && b != c ? a + b + c : (a == b && a != c) || (a == c && a != b) || (b == c && a != b) ? (a + b + c) * (a * a + b * b + c * c) : (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+    }
+}
