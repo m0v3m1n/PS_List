647a52c269b92fd93d36561fa3a76a4983d7c319
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2026-09-17 10:46:48 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/Java/0/181880.1로 만들기/1로 만들기.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public int solution(int[] num_list)
+    {
+        int answer = 0;
+        
+        for(int i = 0; i < num_list.length; i++)
+        {
+            int cnt = 0;
+            
+            while(num_list[i] > 1)
+            {
+                if(num_list[i] % 2 == 0)
+                    num_list[i] /= 2;
+                else
+                    num_list[i] = (num_list[i] - 1) / 2;
+                
+                cnt++;
+            }
+            
+            answer += cnt;
+        }
+        
+        return answer;
+    }
+}
